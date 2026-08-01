b0b77522e8617efc435620786fffb65f208c37f6
Asim <akm02145@gmail.com>
2026-08-01 00:11:35 -0400

    working on implementing turning tuning and functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,107 +1,133 @@
 #include <Arduino.h>
 #include <QTRSensors.h>
+#include <EEPROM.h>
 
-QTRSensors qtr; 
+QTRSensors qtr;
 uint8_t E1 = 5;
 uint8_t M1 = 4;
 uint8_t E2 = 6;
 uint8_t M2 = 7;
-constexpr uint8_t leftMSensor = 8;
-constexpr uint8_t middleSensor = 9;
-constexpr uint8_t rightMSensor = 10;
-constexpr uint8_t leftSensor = 11;
+constexpr uint8_t leftSensor = 9;
+constexpr uint8_t leftMSensor = 10;
+constexpr uint8_t rightMSensor = 11;
 constexpr uint8_t rightSensor = 12;
-constexpr uint8_t sensorCount = 3;
-int defaultSpeed = 105;
+constexpr uint8_t sensorCount = 4;
+int defaultSpeed = 210;
 int32_t targetSensorReading = 1000;
-int32_t sensorList[sensorCount] = {leftMSensor, middleSensor, rightMSensor};
-uint16_t sensorValues[sensorCount] = {leftMSensor, middleSensor, rightMSensor};
+int32_t sensorList[sensorCount] = {leftSensor, leftMSensor, rightMSensor, rightSensor};
+uint16_t sensorValues[sensorCount] = {leftSensor, leftMSensor, rightMSensor, rightSensor};
 int32_t lastError = 0;
+int32_t lastTurnError = 0;
 int previouslyCheckedSensor = 0;
 uint8_t controlInterval = 10;
 float pTerm = 0;
 float iTerm = 0;
 int32_t setPoint = 250;
+bool leftTurnFlag = false;
 bool readLineSensor(int32_t sensorPin);
-void motorController(int32_t pwmValue);
+void motorController(int32_t pwmValue, int32_t directionL, int32_t directionR, bool isTurn);
 void moveLeft(int32_t pwmValue);
 void moveForward(int32_t pwmValue);
 void moveRight(int32_t pwmValue);
 void motorPidController();
 
-
-class Motor {
-  public:
-    uint8_t pwmPin;
-    uint8_t dirPin;
-    Motor(uint8_t pwmPin_, uint8_t dirPin_) {
-      pwmPin = pwmPin_;
-      dirPin = dirPin_;
-    }
-    void begin() {
-      pinMode(pwmPin, OUTPUT);
-      pinMode(dirPin, OUTPUT);
-      digitalWrite(pwmPin, LOW);
-      digitalWrite(dirPin, LOW);
-    }
-    void move(int pwmValue, bool direction) {
-      digitalWrite(pwmPin, LOW);
-      digitalWrite(dirPin, direction);
-      analogWrite(pwmPin, constrain((defaultSpeed + pwmValue), 0, 255));
-    }
-    void stop() {
-      digitalWrite(pwmPin, LOW);
-      digitalWrite(dirPin, LOW);
-      analogWrite(pwmPin, 0);
-    }
+class Motor
+{
+public:
+  uint8_t pwmPin;
+  uint8_t dirPin;
+  Motor(uint8_t pwmPin_, uint8_t dirPin_)
+  {
+    pwmPin = pwmPin_;
+    dirPin = dirPin_;
+  }
+  void begin()
+  {
+    pinMode(pwmPin, OUTPUT);
+    pinMode(dirPin, OUTPUT);
+    digitalWrite(pwmPin, LOW);
+    digitalWrite(dirPin, LOW);
+  }
+  void move(int pwmValue, bool direction)
+  {
+    digitalWrite(pwmPin, LOW);
+    digitalWrite(dirPin, direction);
+    analogWrite(pwmPin, constrain(abs(pwmValue), 0, 255));
+  }
+  void stop()
+  {
+    digitalWrite(pwmPin, LOW);
+    digitalWrite(dirPin, LOW);
+    analogWrite(pwmPin, 0);
+  }
 };
 
 Motor leftMotor(E2, M2);
 Motor rightMotor(E1, M1);
 
-void setup() {
+void setup()
+{
   // Set Timer3 (controls pin 5 / E1) prescaler to 1 → ~31kHz PWM
-  TCCR3B = (TCCR3B & 0b11111000) | 0x01;
+  // TCCR3B = (TCCR3B & 0b11111000) | 0x01;
 
   // Set Timer4 (controls pin 6 / E2) prescaler to 1 → ~31kHz PWM
-  TCCR4B = (TCCR4B & 0b11111000) | 0x01;
+  // TCCR4B = (TCCR4B & 0b11111000) | 0x01;
   Serial.begin(115200);
   leftMotor.begin();
   rightMotor.begin();
   qtr.setTypeRC();
-  qtr.setSensorPins((const uint8_t[]){leftMSensor, middleSensor, rightMSensor}, 3);
+  qtr.setSensorPins((const uint8_t[]){leftSensor, leftMSensor, rightMSensor, rightSensor}, 4);
   qtr.setEmitterPin(2);
   delay(500);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
-
   // 2.5 ms RC read timeout (default) * 10 reads per calibrate() call
   // = ~25 ms per calibrate() call.
-  // Call calibrate() 400 times to make calibration take about 10 seconds.
-  Serial.println("begin calibration");
-  for (uint16_t i = 0; i < 200; i++)
+  // Serial.println("begin calibration");
+  // for (uint16_t i = 0; i < 400; i++)
 
-  {
-    Serial.println(i);
-    qtr.calibrate();
-  }
-  digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
+  // {
+  //   Serial.println(i);
+  //   qtr.calibrate();
+  // }
+  // digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
 
-  // print the calibration minimum values measured when emitters were on
+//  for (uint8_t i = 0; i < sensorCount; i++) {
+//   EEPROM.put(i * 2, qtr.calibrationOn.minimum[i]);
+//   Serial.print(qtr.calibrationOn.minimum[i]);
+//   Serial.print(' ');
+// }
+// Serial.println();
+
+// for (uint8_t i = 0; i < sensorCount; i++) {
+//   EEPROM.put(sensorCount * 2 + i * 2, qtr.calibrationOn.maximum[i]);
+//   Serial.print(qtr.calibrationOn.maximum[i]);
+//   Serial.print(' ');
+// }
+// Serial.println();
+// delay(1000);
+Serial.println("print EPROM");
+delay(1000);
+  qtr.calibrate();
+  // MAX
   for (uint8_t i = 0; i < sensorCount; i++)
   {
-    Serial.print(qtr.calibrationOn.minimum[i]);
+    uint16_t val;
+    EEPROM.get(i * 2, val);
+    qtr.calibrationOn.minimum[i] = val;
+    Serial.print(val);
     Serial.print(' ');
   }
   Serial.println();
-
-  // print the calibration maximum values measured when emitters were on
+  // MIN
   for (uint8_t i = 0; i < sensorCount; i++)
   {
-    Serial.print(qtr.calibrationOn.maximum[i]);
+    uint16_t val;
+    EEPROM.get(sensorCount * 2 + i * 2, val);
+    qtr.calibrationOn.maximum[i] = val;
+    Serial.print(val);
     Serial.print(' ');
   }
-  Serial.println();
   Serial.println();
   delay(1000);
 }
@@ -109,16 +135,19 @@ void setup() {
 void loop()
 {
   qtr.readCalibrated(sensorValues);
-  for (int i = 0; i < sizeof(sensorValues); i++) {
-    //Serial.print(sensorValues[i]);
-    //Serial.print("\t");
+  for (int i = 0; i < sizeof(sensorValues); i++)
+  {
+    // Serial.print(sensorValues[i]);
+    // Serial.print("\t");
   }
-  if (sensorValues[0] == 1000 || sensorValues[1] == 1000 || sensorValues[2] == 1000) {
+  if (sensorValues[0] == 1000 || sensorValues[1] == 1000 || sensorValues[2] == 1000)
+  {
     motorPidController();
   }
-  else {
+  else
+  {
     leftMotor.stop();
-    rightMotor.stop(); 
+    rightMotor.stop();
     // left or right sensors
   }
   // for (uint8_t i = 0; i < sizeof(sensorValues); i++) {
@@ -133,7 +162,6 @@ void loop()
   //   lastCheckedSensor = sensorList[i];
   // }
   Serial.println();
-  delay(10);
 }
 
 // bool readLineSensor(uint8_t sensorPin) {
@@ -142,22 +170,29 @@ void loop()
 //   delayMicroseconds(10);
 //   pinMode(sensorPin, INPUT);
 //   unsigned long start = micros();
-//   while (digitalRead(sensorPin) == HIGH) { 
+//   while (digitalRead(sensorPin) == HIGH) {
 //   }
 //   unsigned long totalTime = micros() - start;
 //   if (totalTime > 2500) {
 //     //return 1;
 //   }
 //   return 0;
-  
+
 // }
 
-void motorController(int32_t pwmValue) {
-    leftMotor.move(-pwmValue, 1);
-    rightMotor.move(pwmValue, 1);
+void motorController(int32_t pwmValue, int32_t directionL, int32_t directionR, bool isTurn)
+{
+  if (!isTurn) {
+    leftMotor.move(-pwmValue + defaultSpeed, directionL);
+    rightMotor.move(pwmValue + defaultSpeed, directionR);
+  } else {
+    leftMotor.move(-pwmValue, directionL);
+    rightMotor.move(pwmValue, directionR);
+  }
+  
 
   // switch(sensorPosition) {
-  //   case leftMSensor: 
+  //   case leftMSensor:
   //     moveLeft(sensorPosition, pwmValue);
   //     break;
   //   case middleSensor:
@@ -171,58 +206,89 @@ void motorController(int32_t pwmValue) {
   // }
 }
 
-void moveForward(int32_t pwmValue) {
-    digitalWrite(E1, LOW);
-    digitalWrite(M1, HIGH);
-    digitalWrite(E2, LOW);
-    digitalWrite(M2, HIGH);
-    Serial.println(defaultSpeed);
-    analogWrite(E1, defaultSpeed);
-    analogWrite(E2, defaultSpeed);
-
+void moveForward(int32_t pwmValue)
+{
+  digitalWrite(E1, LOW);
+  digitalWrite(M1, HIGH);
+  digitalWrite(E2, LOW);
+  digitalWrite(M2, HIGH);
+  Serial.println(defaultSpeed);
+  analogWrite(E1, defaultSpeed);
+  analogWrite(E2, defaultSpeed);
 }
 
-void moveRight(int32_t pwmValue) {
-  
+void moveRight(int32_t pwmValue)
+{
+
   digitalWrite(E1, LOW);
   digitalWrite(M1, LOW);
   digitalWrite(E2, LOW);
   digitalWrite(M2, HIGH);
   analogWrite(E1, 0);
-  analogWrite(E2, constrain((defaultSpeed + pwmValue), 0, 255));
+  analogWrite(E2, constrain(pwmValue, 0, 255));
 }
 
-void moveLeft(int32_t pwmValue) {
+void moveLeft(int32_t pwmValue)
+{
   digitalWrite(E1, LOW);
   digitalWrite(M1, HIGH);
   digitalWrite(E2, LOW);
   digitalWrite(M2, LOW);
-  //Serial.println(defaultSpeed + pwmValue);
-  analogWrite(E1, constrain((defaultSpeed + pwmValue), 0, 255));
+  // Serial.println(defaultSpeed + pwmValue);
+  analogWrite(E1, constrain((defaultSpeed + pwmValue), -255, 255));
   analogWrite(E2, 0);
-
 }
 
-void motorPidController() {
+void motorPidController()
+{
   // Controller gains.
-  float kp = 0.05;
-  float ki = 0.00;
-  float kd = 0.2;
-  uint16_t middleSensorReading = sensorValues[1];
-  // Compute the error (in encoder counts). 
-  int error = sensorValues[0] - sensorValues[2];
-  // Compute the P term.
-  float pTerm = kp * error;
-  // Compute and constrain I term.
-  iTerm += ki * (error + lastError) / 2;
-  iTerm = constrain(iTerm, -255, 255);
-  // Compute D term.
-  float dTerm = kd * (error - lastError);
-  // Compute final output.
-  int32_t controllerOutput = pTerm + iTerm + dTerm;
-  controllerOutput = constrain(controllerOutput, -255, 255);
-  // Update last_error for the next iteration.
-  lastError = error;
-  Serial.println(controllerOutput);
-  motorController(controllerOutput);
+  float kp;
+  float ki;
+  float kd;
+  int error;
+  int tempError;
+  int turnError;
+  if (sensorValues[0] == 1000 && sensorValues[1] == 1000 && sensorValues[2] == 1000 && sensorValues[3] == 1000)
+  {
+    leftMotor.stop();
+    rightMotor.stop();
+  }
+  else if (sensorValues[0] > 900 || sensorValues[3] > 900)
+  {
+    kp = 0.1;
+    ki = 0.00;
+    kd = 0;
+    turnError = sensorValues[0] - sensorValues[3];
+    lastTurnError = turnError;
+    float pTerm = kp * turnError;
+    iTerm += ki * (turnError + lastTurnError) / 2;
+    iTerm = constrain(iTerm, -255, 255);
+    float dTerm = kd * (turnError - lastTurnError);
+    int32_t controllerOutput = pTerm + iTerm + dTerm;
+    controllerOutput = constrain(controllerOutput, -255, 255);
+    lastTurnError = turnError;
+    Serial.println(controllerOutput);
+    if (sensorValues[0] > 900) {
+      motorController(controllerOutput, 0, 1, true);
+    } else {
+      motorController(controllerOutput, 1, 0, true);
+    }
+  }
+  else
+  {
+    kp = 0.1;
+    ki = 0.00;
+    kd = 0;
+    error = sensorValues[1] - (sensorValues[2]);
+    lastError = error;
+    float pTerm = kp * error;
+    iTerm += ki * (error + lastError) / 2;
+    iTerm = constrain(iTerm, -255, 255);
+    float dTerm = kd * (error - lastError);
+    int32_t controllerOutput = pTerm + iTerm + dTerm;
+    controllerOutput = constrain(controllerOutput, -255, 255);
+    lastError = error;
+    Serial.println(controllerOutput);
+    motorController(controllerOutput, 1, 1, false);
+  }
 }
